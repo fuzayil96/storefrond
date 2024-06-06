c66d34fcb521f891fe66de613b51818b23015242
Fuzayil Ubaydullayev <85925862+fuzayil96@users.noreply.github.com>
2024-06-06 14:48:27 +0500

    1

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import './header.css';
-import Search from '../search/search';
+import React from 'react'
+import './header.css'
+import Search from '../search/search'
 
 function Header() {
   return (
     <header className="header-grid">
       <nav>
         <ul>
-          <li><button>Shop</button></li>
-          <li><button>Pages</button></li>
-          <li><button>Blog</button></li>
-          <li><button>Contact</button></li>
-          <li><Search /></li>
+          <li>
+            <button>Shop</button>
+          </li>
+          <li>
+            <button>Pages</button>
+          </li>
+          <li>
+            <button>Blog</button>
+          </li>
+          <li>
+            <button>Contact</button>
+          </li>
+          <li className="seachbar">
+            <Search />
+          </li>
         </ul>
       </nav>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
